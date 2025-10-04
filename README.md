eb6e977c7e1e5e09ce9abd899a7ea60631f5efd6
DudeNr33 <3929834+DudeNr33@users.noreply.github.com>
2025-10-04 16:51:26 +0200

    chore: initial import from MacBook

---
+++ README.md
@@ -0,0 +1,9 @@
+# dotfiles
+
+Configuration for my development tools.
+
+## Install / sync
+
+1. Install `stow` via package manager or homebrew
+2. Run `stow <package>`, e.g. `stow nvim` to create symlinks for
+   the config files of the desired package
---
+++ nvim/.config/nvim/lua/plugins/custom_plugins.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    opts = {
+      size = 20,
+      open_mapping = [[<A-t>]],
+      direction = "float",
+    },
+    version = "*",
+  },
+  -- {
+  --   "xiyaowong/transparent.nvim",
+  --   lazy = false,
+  -- },
+  {
+    "cachebag/nvim-tcss",
+    config = true,
+  },
+}
