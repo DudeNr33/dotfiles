e00a80a3a98be5bcf52ca31548c0c65ef06c4838
DudeNr33 <3929834+DudeNr33@users.noreply.github.com>
2025-10-12 08:02:31 +0200

    fix: adapt robotcode LSP configuration for neovim 0.11 and LazyVim 15.x

--- nvim/.config/nvim/lua/plugins/robot.lua
+++ nvim/.config/nvim/lua/plugins/robot.lua
@@ -3,24 +3,19 @@ return {
   opts = {
     servers = {
       robotcode = {
-        -- default fallback
-        cmd = { "robotcode", "language-server" },
-
         -- dynamically adjust cmd depending on lockfiles in root_dir
-        on_new_config = function(new_config, root_dir)
-          local uv = vim.loop
-
-          if uv.fs_stat(root_dir .. "/uv.lock") then
-            new_config.cmd = { "uv", "run", "robotcode", "language-server" }
-            return
+        cmd = function(dispatchers, config)
+          local local_cmd
+          if vim.uv.fs_stat(config.root_dir .. "/uv.lock") then
+            local_cmd = { "uv", "run", "robotcode", "language-server" }
+          elseif vim.uv.fs_stat(config.root_dir .. "/poetry.lock") then
+            local_cmd = { "poetry", "run", "robotcode", "language-server" }
+          else
+            local_cmd = { "robotcode", "language-server" }
           end
-
-          if uv.fs_stat(root_dir .. "/poetry.lock") then
-            new_config.cmd = { "poetry", "run", "robotcode", "language-server" }
-            return
-          end
-
-          new_config.cmd = { "robotcode", "language-server" }
+          -- uncomment to see what command is actually used
+          -- vim.notify("Cmd used: " .. table.concat(local_cmd, " "))
+          return vim.lsp.rpc.start(local_cmd, dispatchers)
         end,
       },
     },
